896c61da2a60346a77b1dadd786367894a2f49a7
github-world192 <dillen99999999@gmail.com>
2023-09-25 18:40:38 -0700

    Initial Commit

---
+++ swiftui-practice3/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  swiftui-practice3
+//
+//  Created by llll on 2023/9/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ swiftui-practice3/swiftui_practice3App.swift
@@ -0,0 +1,17 @@
+//
+//  swiftui_practice3App.swift
+//  swiftui-practice3
+//
+//  Created by llll on 2023/9/25.
+//
+
+import SwiftUI
+
+@main
+struct swiftui_practice3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
